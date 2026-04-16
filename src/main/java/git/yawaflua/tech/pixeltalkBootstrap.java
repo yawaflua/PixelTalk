fbe93f095da45ba062afccd671a7dfaa69839052
Dmitrii <artemida@yawaflua.tech>
2026-04-16 15:52:32 +0300

    initialize project structure with core plugin systems, database management, and questionnaire flow

---
+++ src/main/java/git/yawaflua/tech/pixeltalkBootstrap.java
@@ -0,0 +1,12 @@
+package git.yawaflua.tech;
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+
+class pixeltalkBootstrap implements PluginBootstrap {
+
+    @Override
+    public void bootstrap(final BootstrapContext context) {
+        // Plugin bootstrap logic
+    }
+}
---
+++ src/main/java/git/yawaflua/tech/pixeltalkLoader.java
@@ -0,0 +1,12 @@
+package git.yawaflua.tech;
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.PluginLoader;
+
+class pixeltalkLoader implements PluginLoader {
+
+    @Override
+    public void classloader(final PluginClasspathBuilder builder) {
+        // Add dynamically loaded libraries here
+    }
+}
